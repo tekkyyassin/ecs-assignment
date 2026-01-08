6ee188fba753c1178583170c6eb8fe285a1fa41b
tekkyyassin <yassindk7@gmail.com>
2026-01-08 23:56:37 +0000

    debug: log IMAGE_TAG in deploy

--- infra/modules/alb/main.tf
+++ infra/modules/alb/main.tf
@@ -32,7 +32,7 @@ resource "aws_security_group" "alb" {
     description      = "All egress"
     from_port        = 0
     to_port          = 0
-    protocol         = "-1"
+    protocol         = "-1" #all outbound traffic
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -37,7 +37,7 @@ resource "aws_route" "public_rt_internet_access" {
 }
 
 resource "aws_subnet" "public_subnets" {
-  count                   = length(var.public_subnets_cidrs)
+  count                   = length(var.public_subnets_cidrs) #consider for_each
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnets_cidrs[count.index]
   availability_zone       = var.azs[count.index]
